9510021d04ada833b8bc14e5dad84a9c3c026dab
Ada Lee <lili545442483@163.com>
2015-06-19 14:48:10 +0800

    add source code ui.R

---
+++ ui.R
@@ -0,0 +1,31 @@
+library(shiny)
+
+# Define UI for dataset viewer application
+shinyUI(fluidPage(
+        
+        # Application title
+        titlePanel("Central Limited Theorem"),
+        
+        # Sidebar with controls to select a dataset and specify the
+        # number of observations to view
+        sidebarLayout(
+                sidebarPanel(
+                        selectInput("distribution", "Choose a distribution:", 
+                                    choices = c("Binomial", "Exponential", "F",
+                                                "Logistic", "Poisson", "Student t", "Uniform")),
+                        
+                        sliderInput("num", "Number of points need to mean:",
+                                    min = 1, max = 40, step = 39, value = 1)
+                ),
+                
+                # Show a summary of the dataset and an HTML table with the 
+                # requested number of observations
+                mainPanel(
+                        verbatimTextOutput("distFunc"),
+                        
+                        #tableOutput("view"),
+                        
+                        plotOutput("distPlot")
+                )
+        )
+))
